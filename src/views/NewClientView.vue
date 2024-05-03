d7355cb983f4531048ca87a102740072092837a4
Roberto Mazzarello <rmazzarello@gmail.com>
2024-05-03 18:15:26 -0300

    Agregamos componentes, router, instalamos axios, y configuramos formkit

---
+++ src/views/NewClientView.vue
@@ -0,0 +1,111 @@
+<script setup>
+    import { reactive, onMounted, ref } from 'vue'
+    import axios from 'axios'
+    import RouterLink from '../components/UI/RouterLink.vue'
+    import Heading from '@/components/UI/Heading.vue'
+    import { FormKit } from '@formkit/vue'
+
+    defineProps({
+        title: {
+            type: String
+        }
+    })
+
+    const clients = ref([])
+    const formData = reactive({
+        name: 'rober',
+        lastName: '',
+        email: '',
+        phone: '',
+        company: '',
+        position: ''
+    })
+
+    onMounted(() => {
+        axios('http://localhost:4000/clients')
+            .then(({data}) => clients.value = data)
+            .catch( error => console.log("Hubo un error"))
+    })
+
+    const handleSubmit = (data) => {
+        console.log(data)
+    }
+</script>
+
+<template>
+    <div>
+        <div class="flex justify-end">
+            <RouterLink to="inicio">
+                Volver
+            </RouterLink>
+        </div>
+        <Heading>{{ title  }}</Heading>
+        <div class="mx-auto mt-10 bg-white shadow">
+            <div class="mx-auto md:w-2/3 py-20 px-6">
+                <FormKit
+                    type="form"
+                    submit-label="Agregar Cliente"
+                    incomplete-message="Error campos obligatorios incompletos"
+                    @submit="handleSubmit"
+                >
+                    <FormKit 
+                        type="text"
+                        label="Nombre"  
+                        name="name" 
+                        placeholder="Nombre del cliente*" 
+                        validation="required"
+                        :validation-messages="{ required: 'El Nombre del cliente es obligatorio' }"
+                    />
+                    <FormKit 
+                        type="text"
+                        label="Apellido" 
+                        name="lastName"  
+                        placeholder="Apellido del cliente*" 
+                        validation="required"
+                        :validation-messages="{ required: 'El Apellido del cliente es obligatorio' }"
+                    />
+                    <FormKit 
+                        type="email"
+                        label="Email"  
+                        name="email" 
+                        placeholder="Email del cliente*" 
+                        validation="required | email"
+                        :validation-messages="{ 
+                            required: 'El Email del cliente es obligatorio', 
+                            email: 'El correo ingresado no es válido' 
+                        }"
+                    />
+                    <FormKit 
+                        type="text"
+                        label="Teléfono"
+                        name="phone"   
+                        placeholder="Teléfono: XXX-XXX-XXXX" 
+                        validation="?matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+                        :validation-messages="{ matches: 'El formato no es válido'}"
+                    />
+                    <FormKit 
+                        type="text"
+                        label="Empresa"  
+                        name="company" 
+                        placeholder="Empresa del cliente" 
+                    />
+                    <FormKit 
+                        type="text"
+                        label="Puesto" 
+                        name="position"  
+                        placeholder="Puesto del cliente" 
+                    />
+                    
+                    <p class="flex justify-end">* Los campos son obligatorios</p>
+                    
+                </FormKit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .formkit-wrapper {
+        max-width: 100%;
+    }
+</style>
